4f5dacc030e4dbb3c6eedaa72110497b46da3b44
Nazira17 <you@example.com>
2019-02-25 10:58:52 +0600

    Add draft code app.py

--- api.py
+++ api.py
@@ -1,1 +1,93 @@
-lala
+from flask import Flask, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from tempmail import TempMail
+
+# ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+app = Flask(__name__)
+
+
+# def allowed_file(file):
+#     return '.' in filename and \
+#            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def get_img_from_api(file):
+    if request.method == 'GET':
+            # check if the post request has the file part
+            if 'file' not in request.files:
+                flash('No file part')
+                return redirect(request.url)
+            else:
+                uploaded_file = request.files['file']
+                # if uploaded_file:
+                # return '.' in filename and \
+                # filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+
+
+def get_mail(key):
+    tm = TempMail()
+    email = tm.get_email_address()  # v5gwnrnk7f@gnail.pw  создаем почту
+    key = tm.get_mailbox(email)  # list of emails получаем письма из почты
+
+
+def post_tinyjpg(file, key):
+    tinify.key = "YOUR_API_KEY"
+    source = tinify.from_file(uploaded_file)
+    source.to_file(uploaded_file)
+    # compressions_this_month = tinify.compression_count
+    return
+
+
+def get_tinyjpg(file):
+    pass
+
+
+def patch_db():
+    pass
+
+
+@app.route('/', methods=['GET', 'POST', 'PATCH'])
+    img = get_img_from_api()
+    mail = get_mail()
+    tinyjpg = post_tinyjpg()
+    key = patch_db()
+    send_img = send_img_to_api()
+
+
+
+
+
+
+
+
+
+# def compress_image(file):
+#     """Compress image size via API"""
+
+#     # keep track how many times API have been used
+#     if tinify.key == "ZPF88NBkpcTy9m2Bsz1qRq4ZRMf4H9WP":
+#         db.execute("UPDATE api_count SET count1 = count1 + 1")
+#     else:
+#         db.execute("UPDATE api_count SET count2 = count2 + 1")
+
+#     counter = db.execute("SELECT * FROM api_count")[0]
+
+#     # don't compress image if API request limit is reached
+#     if counter["count1"] == 495 or counter["count2"] == 495:
+#         return "Limit reached"
+
+
+
+
+
+
+
+
+
+
+
+    if __name__ == '__main__':
+        app.run(debug=True)
