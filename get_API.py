72a23a2dd6b845d85e2cb3e8b23e053626c706db
KorkaYess <karina.yesbukenova@gmail.com>
2019-02-28 12:34:02 +0600

    Rewrite the CSS selector fo inputs

--- get_API.py
+++ get_API.py
@@ -13,11 +13,14 @@ def get_email(session):
 
 def registration(session):
     t = session.get('https://tinypng.com/developers')
-    name = t.html.find('#Your full name', first=True)
-    email = t.html.find('#Your email address', first=True)
-    submit = t.html.find('#submit', first=True)
     random_name = names.get_first_name()
-    email = get_email()
-    name.attrs['name'] = random_name
-    email.attrs['name'] = email
-    submit.attrs['autocomplete'] = 'on'
+    email = get_email(session)
+    inputs = t.html.find(':input')
+    print(random_name)
+    print(email)
+    inputs[0].attrs['required'] = random_name
+    inputs[1].attrs['required'] = email
+    inputs[2].attrs['autocomplete'] = 'on'
+
+
+registration(session)
