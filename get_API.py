74d89165dfeb137a634ef4a20e628a4f7a9ce30e
KorkaYess <karina.yesbukenova@gmail.com>
2019-02-28 17:32:40 +0600

    Finish registration via selenium; start confirmation

--- get_API.py
+++ get_API.py
@@ -1,26 +1,39 @@
 from requests_html import HTMLSession
+from selenium import webdriver
 import names
 
-
 session = HTMLSession()
+driver = webdriver.Chrome('/home/korka/Roborin/image_optimizer/chromedriver')
+r = session.get('https://temp-mail.org/ru/')
+url = 'https://temp-mail.org/ru/'
 
 
-def get_email(session):
-    r = session.get('https://temp-mail.org/ru/')
+def get_email(url):
+    r = session.get(url)
     getmail = r.html.find('#mail.opentip', first=True)
     return getmail.attrs['value']
 
 
-def registration(session):
-    t = session.get('https://tinypng.com/developers')
+def registration():
+    url = 'https://tinypng.com/developers'
+    driver.get(url)
     random_name = names.get_first_name()
-    email = get_email(session)
-    inputs = t.html.find(':input')
+    email = get_email(url)
     print(random_name)
     print(email)
-    inputs[0].attrs['required'] = random_name
-    inputs[1].attrs['required'] = email
-    inputs[2].attrs['autocomplete'] = 'on'
+    driver.find_element_by_css_selector('input[name="fullName"]').\
+        send_keys(random_name)
+    driver.find_element_by_css_selector('input[name="mail"]').send_keys(email)
+    driver.find_element_by_css_selector('input[type=submit]').click()
 
 
-registration(session)
+def confirmation(url):
+    driver.get(url)
+    registration()
+    driver.find_element_by_class_name('title-subject').click()
+    driver.find_element_by_css_selector('input[target="_blank"]').click()
+    pass
+
+
+if __name__ == '__main__':
+    confirmation(url)
