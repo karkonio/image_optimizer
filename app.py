1667132a3e303049e387bb1492d97ee6f212ed98
Nazira17 <you@example.com>
2019-04-03 09:49:49 +0600

    Nothing

--- app.py
+++ app.py
@@ -2,14 +2,16 @@ from flask import Flask, request, send_file, Response
 import tinify
 from flask import session
 from flask_sqlalchemy import SQLAlchemy
-import io
 from api_key import api
+import io
 
 
 app = Flask(__name__)
 app.secret_key = 'secret key'
 app.config['SQLALCHEMY_DATABASE_URI'] = \
     'postgresql://postgres:password@localhost:5432/api'
+
+
 db = SQLAlchemy(app)
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
@@ -37,14 +39,18 @@ def upload_file():
         print(request.files)
         if file and allowed_file(file.filename):
             return file
-    return 'Hello'
+        elif file:
+            raise Exception('File type not allowed')
+        else:
+            raise Exception('No file found in request')
 
 
-def post_tinyjpg(source, db):
+def post_tinyjpg(source):
+    print(2)
     try:
         get_key = api_keys.query.filter(api_keys.counter < 500).first()
         tinify.key = get_key.key
-    except:
+    except Exception:
         new_api_key(db)
         get_key = api_keys.query.filter(api_keys.counter == 0).first()
         tinify.key = get_key.key
@@ -52,8 +58,10 @@ def post_tinyjpg(source, db):
     result_data = tinify.from_buffer(source_data).to_buffer()
     get_key.counter = get_key.counter + 1
     db.session.commit()
+    print(result_data)
     response = io.BytesIO(result_data)
     response.filename = source.filename
+    print(response.filename)
     return response
 
 
@@ -64,30 +72,27 @@ def new_api_key(db):
     db.session.commit()
 
 
-@app.route('/')
-def index():
-    return 'Hello'
-
-
 @app.route('/upload', methods=['POST', 'GET'])
 def root():
     if 'file' not in request.files:
-        try:
-            return Response('No file part',
-                            status=422,
-                            mimetype='application /json')
-        except Exception:
-            pass
+        return Response('{"error": "No file part"}',
+                        status=422,
+                        mimetype='application/json')
+
     else:
         try:
             img = upload_file()
+            print(1)
             img_small = post_tinyjpg(img)
+            print(3)
             return send_file(img_small, attachment_filename=img_small.filename)
-        except Exception:
-            return Response('Picture format is not correct',
+            print(4)
+        except Exception as e:
+            print(e)
+            return Response('{"error": "Picture format is not correct"}',
                             status=422,
-                            mimetype='application /json')
+                            mimetype='application/json')
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1')
+    app.run(debug=True)
